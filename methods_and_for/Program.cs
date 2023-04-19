f4c1c43d4e66e3fd82d86c4cfbe1c117947887bd
unknown <Ivan>
2023-04-19 23:38:25 +0700

    initial commit

---
+++ methods_and_for/Program.cs
@@ -0,0 +1,21 @@
+﻿string text = "я очень красивый мальчик" + "и еще очень умный";  
+
+string AnotherText(string text, char oldValue, char newValue)
+{
+ string result = String.Empty;
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+    if (text[i] == oldValue)
+    {
+        result = result + $"{newValue}";
+    }
+    else
+    {
+        result = result + $"{text[i]}";
+    }
+}
+ return result;
+}
+ string newText = AnotherText(text, ' ', '|');
+ Console.WriteLine($"{newText}");
